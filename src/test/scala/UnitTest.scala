3f62f863b912ac8b634d6aa78440b078ee296851
DS-Daniel <daniel.sciarra@heig-vd.ch>
2022-04-09 16:01:05 +0200

    more tests

--- src/test/scala/UnitTest.scala
+++ src/test/scala/UnitTest.scala
@@ -1,37 +1,193 @@
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest._
+import flatspec.AnyFlatSpec
+import matchers.should._
 import Chat.{AnalyzerService, Parser, TokenizerService}
 import Data.*
 import Utils.{Dictionary, SpellCheckerImpl, SpellCheckerService}
 
-class UnitTest extends AnyFlatSpec with BeforeAndAfterAll {
+class UnitTest extends AnyFlatSpec with Matchers {
 
-  val spellCheckerSvc: SpellCheckerService = new SpellCheckerImpl(Dictionary.dictionary)
-  val tokenizerSvc: TokenizerService = new TokenizerService(spellCheckerSvc)
-  val productSvc: ProductService = new ProductImpl()
-  val sessionSvc: SessionService = new SessionImpl()
-  val accountSvc: AccountService = new AccountImpl()
-  val analyzerSvc: AnalyzerService = new AnalyzerService(productSvc, accountSvc)
-  val session: Session = sessionSvc.create()
+  val spellCheckerSvc: SpellCheckerService = SpellCheckerImpl(Dictionary.dictionary)
+  val tokenizerSvc: TokenizerService = TokenizerService(spellCheckerSvc)
+  val analyzerSvc: AnalyzerService = AnalyzerService(ProductImpl(), AccountImpl())
+  val session: Session = SessionImpl().create()
 
-  "Request" should "match" in {
-    val req = "Bonjour, je suis assoiffé !"
-    val tokenized = tokenizerSvc.tokenize(req.toLowerCase)
-    val parser = new Parser(tokenized)
-    val expr = parser.parsePhrases()
-    val actual = analyzerSvc.reply(session)(expr)
-    val expected = "Eh bien, la chance est de votre côté, car nous offrons les meilleures bières de la région !"
-    assert(actual.equals(expected))
+  "Analyzer" should "process EtatAme" in {
+    var req = "Bonjour, je suis assoiffé !"
+    var tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    var expr = Parser(tokenized).parsePhrases()
+    var actual = analyzerSvc.reply(session)(expr)
+    var expected = "Eh bien, la chance est de votre côté, car nous offrons les meilleures bières de la région !"
+    actual shouldBe expected
+
+    req = "Bonjour, je suis affamé !"
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Pas de soucis, nous pouvons notamment vous offrir des croissants faits maisons !"
+    actual shouldBe expected
   }
 
-  it should "match1" in {
-    val req = "Bonjour, je suis affamé !"
-    val tokenized = tokenizerSvc.tokenize(req.toLowerCase)
-    val parser = new Parser(tokenized)
-    val expr = parser.parsePhrases()
-    val actual = analyzerSvc.reply(session)(expr)
-    val expected = "Pas de soucis, nous pouvons notamment vous offrir des croissants faits maisons !"
-    assert(actual.equals(expected))
+  it should "ask for identification" in {
+    var req = "J’aimerais commander 3 bières PunkIPAs !"
+    var tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    var expr = Parser(tokenized).parsePhrases()
+    var actual = analyzerSvc.reply(session)(expr)
+    val expected = "Veuillez d’abord vous identifier."
+    actual shouldBe expected
+
+    req = "J'aimerais connaitre mon solde."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    actual shouldBe expected
   }
-  
+
+  it should "process user requests" in {
+    var req = "Bonjour, je suis _Bob."
+    var tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    var expr = Parser(tokenized).parsePhrases()
+    var actual = analyzerSvc.reply(session)(expr)
+    var expected = "Hola, bob!"
+    actual shouldBe expected
+
+    req = "Combien coûte 1 bière PunkIPA ?"
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Cela coûte CHF 3.0."
+    actual shouldBe expected
+
+    req = "J'aimerais connaitre mon solde."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Le montant actuel de votre solde est de CHF 30.0."
+    actual shouldBe expected
+
+    req = "Je veux commander 2 bières PunkIPAs et 1 bière Ténébreuse."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Voici donc 2 punkipa et 1 tenebreuse ! Cela coûte CHF 10.0 et votre nouveau solde est de CHF 20.0."
+    actual shouldBe expected
+
+    req = "Je voudrais commander 1 croissant."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Voici donc 1 croissant maison ! Cela coûte CHF 2.0 et votre nouveau solde est de CHF 18.0."
+    actual shouldBe expected
+  }
+
+  it should "process mutliple users" in {
+    var req = "Bonjour, je suis _Bob."
+    var tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    var expr = Parser(tokenized).parsePhrases()
+    var actual = analyzerSvc.reply(session)(expr)
+    var expected = "Hola, bob!"
+    actual shouldBe expected
+
+    req = "J'aimerais commander 4 bieres JackHammer"
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Voici donc 4 jackhammer ! Cela coûte CHF 12.0 et votre nouveau solde est de CHF 18.0."
+    actual shouldBe expected
+
+    req = "Bonjour, je suis _Alice."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Hola, alice!"
+    actual shouldBe expected
+
+    req = "Je suis affamé !"
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Pas de soucis ! Nous pouvons vous offrir des croissants faits maisons !"
+    actual shouldBe expected
+
+    req = "Je veux commander 2 croissants cailler."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Voici donc 2 croissant cailler ! Cela coûte CHF 4.0 et votre nouveau solde est de CHF 26.0."
+    actual shouldBe expected
+
+    req = "Je veux connaître mon solde."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Le montant actuel de votre solde est de CHF 26.0."
+    actual shouldBe expected
+
+    req = "je suis _Bob."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Hola, bob!"
+    actual shouldBe expected
+
+    req = "Je veux connaître mon solde."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Le montant actuel de votre solde est de CHF 18.0."
+    actual shouldBe expected
+
+    req = "J'aimerais commander 18 bières Farmer."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Voici donc 18 farmer ! Cela coûte CHF 18.0 et votre nouveau solde est de CHF 0.0."
+    actual shouldBe expected
+  }
+
+  it should "process OU request correctly" in {
+    var req = "Quel est le prix de 1 bière PunkIPA ou 1 croissant ?"
+    var tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    var expr = Parser(tokenized).parsePhrases()
+    var actual = analyzerSvc.reply(session)(expr)
+    var expected = "Cela coûte CHF 2.0."
+    actual shouldBe expected
+
+    req = "je suis _Bob."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Hola, bob!"
+    actual shouldBe expected
+
+    req = "J'aimerais commander 1 bière wittekop ou 1 croissant"
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Voici donc 1 wittekop ! Cela coûte CHF 2.0 et votre nouveau solde est de CHF 28.0."
+    actual shouldBe expected
+  }
+
+  it should "process ET request correctly" in {
+    var req = "Combien coûtent 4 bières Farmers et 6 croissants cailler?"
+    var tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    var expr = Parser(tokenized).parsePhrases()
+    var actual = analyzerSvc.reply(session)(expr)
+    var expected = "Cela coûte CHF 16.0."
+    actual shouldBe expected
+
+    req = "je suis _Bob."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Hola, bob!"
+    actual shouldBe expected
+
+    req = "J'veux commander 2 bières Farmers et 1 bière Jackhammer."
+    tokenized = tokenizerSvc.tokenize(req.toLowerCase)
+    expr = Parser(tokenized).parsePhrases()
+    actual = analyzerSvc.reply(session)(expr)
+    expected = "Voici donc 2 Farmers et 1 Jackhammer ! Cela coûte CHF 5.00 et votre nouveau solde est de CHF 25.0."
+    actual shouldBe expected
+  }
 }
