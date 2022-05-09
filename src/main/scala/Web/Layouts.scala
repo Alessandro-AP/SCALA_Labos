b96417be1124cfe75c6f181013122921bd548308
Alessandro-AP <alessandro.parrinoap@gmail.com>
2022-05-09 23:18:25 +0200

    Serve static resources files (Step 1) and add a hello world html page with ScalaTags

--- src/main/scala/Web/Layouts.scala
+++ src/main/scala/Web/Layouts.scala
@@ -8,8 +8,28 @@ import scalatags.Text.all._
 import scalatags.Text.tags2
 
 /**
- * Assembles the method used to layout ScalaTags
- */
+  * Assembles the method used to layout ScalaTags
+  */
 object Layouts:
-    // You can use it to store your methods to generate ScalaTags.
+  // You can use it to store your methods to generate ScalaTags.
+  def homepage = {
+    html(
+      head(
+        meta(charset := "utf-8"),
+        meta(name := "viewport", content := "initial-scale = 1.0,maximum-scale = 1.0"),
+        link(
+          rel := "stylesheet",
+          href := "static/css/main.css"
+        ),
+        script(src := "static/js/main.js"),
+
+      ),
+      body(margin := 0, backgroundColor := "#f8f8f8")(
+        div(
+          h1(id := "title", "This is a title"),
+          p("This is a big paragraph of text")
+        )
+      )
+    )
+  }
 end Layouts
